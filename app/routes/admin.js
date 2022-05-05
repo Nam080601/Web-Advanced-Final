4a8b553b50a55f6b6c845fa294b685358412287b
Lê Tuấn Anh <76466761+githubtuananh@users.noreply.github.com>
2022-05-05 21:01:25 +0700

    User Function

--- app/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const adminController = require('../controllers/adminController');
-
-// router.get("/:slug", adminController.profile);
-router.get("/", adminController.index);
-
-module.exports = router;
--- app/routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  const locals = { title: "Home" };
-  res.render("login", locals);
-});
-
-module.exports = router;
