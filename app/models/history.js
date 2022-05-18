033291ef4dce56bae1c6ab356dec10a65c9dafae
Luu Kien Van <73321315+LuuKienVan@users.noreply.github.com>
2022-05-18 13:29:24 +0700

    Add files via upload

--- app/models/history.js
+++ app/models/history.js
@@ -2,34 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const History = new Schema({
-    type: {
-        type: String,
-        required: true,
-    },
-    // ------------ if rechage or withdraw or buy cards ------------
-    card_id: {
-        type: String,
-    },
-    //--------------------------------------------------------------
-    // ------------ if transfer monney ----------------
-    receiver_phone_number: {
-        type: String,
-    },
+    username: {type: String, required: true},
+    type: { type: String, required: true },
+    // ------------ for transfer monney ----------------
+    receiver_phone_number: { type: String },
     // ------------------------------------------------
-    money: {
-        type: Number,
-        required: true,
-    },
-    date: {
-        type: String,
-        required: true,
-    },
-    message: {
-        type: String,
-    },
-    status: {
-        type: String,
-    }
+    money: { type: Number, required: true },
+    date: { type: Date, default: Date.now},
+    message: { type: String },
+    status: { type: String } //Success, Cancelled, Pending
 })
 
 module.exports = mongoose.model("History", History);
